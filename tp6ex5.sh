6c66354d1330a4caeac7cdea4cb16fed0b3f3b41
xakiv <niark.hipps@gmail.com>
2014-11-12 17:15:48 +0100

    Update tp6ex5.sh

--- tp6ex5.sh
+++ tp6ex5.sh
@@ -2,10 +2,7 @@
 cmp=1
 for i in $*
 do
-  echo argument $cmp \=  $i\,
-  cmp=$((cmp+1))
-  if [ cmp -eq $#]
-  then
-    echo \. Il y a $# arguments
-  fi
+	echo -n \ argument $cmp \= $i\,
+	cmp=$((cmp+1))	
 done
+echo \ Il y a $# arguments\.
