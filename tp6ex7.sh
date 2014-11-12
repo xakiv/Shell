9a511b13c4770288e586f3836f15577a3e2b11b7
xakiv <niark.hipps@gmail.com>
2014-11-12 19:20:50 +0100

    Create tp6ex7.sh

---
+++ tp6ex7.sh
@@ -0,0 +1,12 @@
+echo Liste des commandes dispo :
+echo 1 - Date
+echo 2 - Liste des connectés
+echo 3 - Liste des fichiers
+echo 4 - Repertoire courant
+read ans
+case $ans in
+1 ) date +%A\ %d\ %B^C;;
+2 ) ;;
+3 ) ;;
+4 ) ;;
+esac
