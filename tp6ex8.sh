75fc1930232e8deedd6b43927be17f32d82e073a
xakiv <niark.hipps@gmail.com>
2014-11-14 15:49:21 +0100

    Create tp6ex8.sh

---
+++ tp6ex8.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+echo tapez votre string ou tapez \"sortir\" pour quitter\:
+read ans
+while [ $ans != sortir]
+do
+  echo tapez votre string ou tapez \"sortir\" pour quitter\:
+  read ans
+done
