b704d8c182f7e1c374e196db871d928ff8d8b4f6
xakiv <niark.hipps@gmail.com>
2014-11-14 16:34:30 +0100

    Update tp6ex9.sh

--- tp6ex9.sh
+++ tp6ex9.sh
@@ -1,9 +1,23 @@
 #! /bin/sh
-echo Indiquez votre string \:
+echo Liste des commandes dispo :
+echo 1 - Date
+echo 2 - Liste des connectés
+echo 3 - Liste des fichiers
+echo 4 - Repertoire courant
 read ans
-while [ "$ans" != "sortir" ]
-do
-	 echo tapez votre string ou tapez sortir pour quitter\:
-	 read ans
-	 echo Vous avez \écrit $ans
+while [ $ans -eq 1 -o $ans -eq 2 -o $ans -eq 3 -o $ans -eq 4 ]; do
+	echo ""
+	case $ans in
+	1 ) date +%A\ %d\ %B;;
+	2 ) awk -F':' '{ print $1}' /etc/passwd ;;
+	3 ) dir ;;
+	4 ) pwd ;;
+	esac
+	echo ""
+	echo Liste des commandes dispo \:
+	echo 1 - Date
+	echo 2 - Liste des connectés
+	echo 3 - Liste des fichiers
+	echo 4 - Repertoire courant
+	read ans
 done
