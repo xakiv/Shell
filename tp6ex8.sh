99348f90412a2717c4bd9f2130f1a2e71cd2dc0a
xakiv <niark.hipps@gmail.com>
2014-11-14 16:14:11 +0100

    Update tp6ex8.sh

--- tp6ex8.sh
+++ tp6ex8.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
-echo tapez votre string ou tapez \"sortir\" pour quitter\:
+echo Indiquez votre string \:
 read ans
-while [ $ans != sortir]
+while [ "$ans" != "sortir" ]
 do
-  echo tapez votre string ou tapez \"sortir\" pour quitter\:
-  read ans
+	 echo tapez votre string ou tapez sortir pour quitter\:
+	 read ans
+	 echo Vous avez \écrit $ans
 done
