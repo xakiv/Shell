ffc5491569735844b84ecb1e341858c943ac7213
xakiv <niark.hipps@gmail.com>
2014-11-14 16:48:16 +0100

    Create tp7ex1.sh

---
+++ tp7ex1.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+fct () {
+	var2="variable 2"
+	echo "\nDans fonction"
+	echo "\$1 = $A"
+	echo "var1 = $var1"
+	echo "var2 = $var2"
+	echo "fin fonction\n"
+}
+
+var1="variable 1"
+echo "echo dans script principal"
+echo "\$1 = $1"
+echo "var1 = $var1"
+fct $var1
+echo "var2 = $var2"
+echo "fin script principal"
