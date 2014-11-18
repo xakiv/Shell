9010c559a4c87152803cc3c39a6598a73edd9b43
xakiv <niark.hipps@gmail.com>
2014-11-18 18:29:13 +0100

    Update tp7ex3.sh

--- tp7ex3.sh
+++ tp7ex3.sh
@@ -1,5 +1,12 @@
 #! /bin/sh
 fact () {
-for cmp in seq $1; do
-var=$(( $1 * $cmp ))
+pouet=$(( $1 - 1 ))
+for cpt in seq $1
+do
+res=$(( $cpt * $pouet ))
+done
+return $res
 }
+
+fact $1
+echo la factorielle de $1 est $res
